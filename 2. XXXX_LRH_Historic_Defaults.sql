5eb13805f0dd7b2d1f2cd2f72d645fd2e2c049f0
vanrhel2 <luciano.vanrheenen2@mail.dcu.ie>
2017-08-30 13:21:53 +0100

    Rename XXXX_LRH_Historic_Output.sql to 2. XXXX_LRH_Historic_Defaults.sql

---
+++ 2. XXXX_LRH_Historic_Defaults.sql
@@ -0,0 +1,31 @@
+﻿/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT distinct a.[DATE]
+				,b.[TOTAL_ACCOUNTS]
+				,c.[DEFAULT_ACCOUNTS]
+  FROM [ETZ335IH_PROJECT_JERSEY].[dbo].[XXXX_LRH_Historic_Master_Final] a
+
+  LEFT JOIN (
+
+SELECT distinct [DATE]
+      ,count(*) as [TOTAL_ACCOUNTS]
+  FROM [ETZ335IH_PROJECT_JERSEY].[dbo].[XXXX_LRH_Historic_Master_Final]
+
+  group by [DATE]
+  ) b
+  on a.[DATE] = b.[DATE]
+
+ LEFT JOIN (
+
+  SELECT distinct [DATE]
+      ,count(*) as [DEFAULT_ACCOUNTS]
+  FROM [ETZ335IH_PROJECT_JERSEY].[dbo].[XXXX_LRH_Historic_Master_Final]
+  
+  where [1_MONTH_DEFAULT_FLAG] = 'Y'
+  
+  group by [DATE]
+  ) c
+  on a.[DATE] = c.[DATE]
+  
+  where a.[DATE] <> '2013-12-31'
+  
+  order by [DATE]
